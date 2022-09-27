bbffad5b5ff32ff80deee2610ec2f19b9f9f8c9f
Bruno Venâncio de Souza e Silva <brunosilva.5934@aluno.saojudas.br>
2022-09-27 01:49:44 -0300

    Adicionada página de edição de registro

--- routes/list.py
+++ routes/list.py
@@ -8,3 +8,8 @@ list_clients = Blueprint("list", __name__)
 def client_list():
     db_query = User.query.all()
     return render_template("list.html", clientes=db_query), 200
+
+@list_clients.route("/list/update/<client_id>")
+def update_form(client_id=0):
+    client = User.query.filter_by(id=client_id).first()
+    return render_template("update_c.html", client=client), 200
